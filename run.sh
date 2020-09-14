f9ac38dbadb573ad7e864d0679c092a44056fdb0
Andrey Gangan <a.gangan@gmail.com>
2020-09-14 20:38:07 +0300

    sript

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
 echo "Welcome to my django learning projest"
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+sudo /etc/init.d/mysql start
