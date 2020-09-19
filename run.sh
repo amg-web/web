6ba072c0d6d82f6a19b64eea74b478d8d096a792
anmg <a.gangan@gmail.com>
2020-09-19 19:28:46 +0300

    nginx

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 echo "Welcome to my django learning projest"
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/box.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/box
+rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/gunicorn.conf
 #sudo /etc/init.d/gunicorn restart
